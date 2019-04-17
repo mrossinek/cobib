bf2e3d2b315ee0c178940c82605e38a9e6b1c4ac
mrossinek <rmax@ethz.ch>
2019-04-17 18:13:06 +0200

    Add some comments and doc strings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# {{{ IMPORTS
 from subprocess import Popen
 import argparse
 import configparser
@@ -7,10 +8,18 @@ import re
 import requests
 import sqlite3
 import sys
+# }}}
 
+
+# {{{ GLOBAL VARIABLES
+# API and HEADER settings according to this resource
+# https://crosscite.org/docs.html
 API_URL = "https://doi.org/"
 HEADER = {'Accept': "application/x-bibtex"}
+# DOI regex used for matching DOIs
 DOI_REGEX = r'(10\.[0-9a-zA-Z]+\/(?:(?!["&\'])\S)+)\b'
+# custom database keys which are not part of the biblatex default keys
+# this dict may also hold all those keys that require special parameters
 TABLE_KEYS = {
     'doi':      "primary key not null",
     'type':     "not null",
@@ -19,6 +28,8 @@ TABLE_KEYS = {
     'tags':     "",
     'abstract': ""
     }
+# biblatex default types and required values taken from their docs
+# https://ctan.org/pkg/biblatex
 BIBTEX_TYPES = {
     'article': ['author', 'title', 'journal', 'year'],
     'book': ['author', 'title', 'year'],
@@ -28,13 +39,27 @@ BIBTEX_TYPES = {
     'thesis': ['author', 'title', 'type', 'institution', 'year'],
     'unpublished': ['author', 'title', 'year']
     }
+# }}}
 
+
+# {{{ CONFIG
 config = configparser.ConfigParser()
 config.read('config.ini')
 conf_database = dict(config['DATABASE'])
+# }}}
 
 
+# {{{ ARGUMENT FUNCTIONS
 def init(args):
+    """
+    Initializes the sqlite3 database at the configured location.
+    A single table is used which is named in the config file.
+    The initial columns correspond to all minimally required values of the
+    default biblatex types plus the custom keys defined initially.
+    All fields are TEXT fields and do not have any special attributes. If an
+    entry must not be NULL it should be declared in the TABLE_KEYS dictionary
+    with its according parameters.
+    """
     conn = sqlite3.connect(conf_database['path'])
     cmd = "create table "+conf_database['table']+"(\n"
     for type, keys in BIBTEX_TYPES.items():
@@ -49,6 +74,9 @@ def init(args):
 
 
 def list(args):
+    """
+    Lists all entries in the database.
+    """
     conn = sqlite3.connect(conf_database['path'])
     cursor = conn.execute("SELECT rowid, doi, label FROM "+conf_database['table'])
     for row in cursor:
@@ -56,6 +84,9 @@ def list(args):
 
 
 def show(args):
+    """
+    Prints the details of a selected entry in bibtex format to stdout.
+    """
     conn = sqlite3.connect(conf_database['path'])
     conn.row_factory = sqlite3.Row
     cursor = conn.execute("SELECT * FROM "+conf_database['table']+" WHERE rowid = "+str(args.id))
@@ -64,6 +95,9 @@ def show(args):
 
 
 def open(args):
+    """
+    Opens the associated file of an entry with xdg-open.
+    """
     conn = sqlite3.connect(conf_database['path'])
     conn.row_factory = sqlite3.Row
     cursor = conn.execute("SELECT * FROM "+conf_database['table']+" WHERE rowid = "+str(args.id))
@@ -76,6 +110,9 @@ def open(args):
 
 
 def add(args):
+    """
+    Adds new entries to the database.
+    """
     dois = []
     def flatten(l): return [item for sublist in l for item in sublist]
     if args.pdf is not None:
@@ -97,9 +134,17 @@ def add(args):
         assert(re.match(DOI_REGEX, doi_str))
         page = requests.get(API_URL+doi_str, headers=HEADER)
         insert_entry(page.text, file)
+# }}}
 
 
+# {{{ HELPER FUNCTIONS
 def insert_entry(bibtex: str, pdf: str = None):
+    """
+    Inserts an entry into the database.
+    This function has the following side effects:
+    * any missing key columns are inserted into the database
+    * if a duplicate entry appears to exist, nothing happens
+    """
     # load database info
     conn = sqlite3.connect(conf_database['path'])
     cursor = conn.execute("PRAGMA table_info("+conf_database['table']+")")
@@ -135,6 +180,9 @@ def insert_entry(bibtex: str, pdf: str = None):
 
 
 def bibtex_to_dict(bibtex: str):
+    """
+    Converts a bibtex formatted string into a dictionary of key-value pairs.
+    """
     entry = {}
     lines = bibtex.split('\n')
     entry['type'] = re.findall(r'^@([a-zA-Z]*){', lines[0])[0]
@@ -146,14 +194,19 @@ def bibtex_to_dict(bibtex: str):
 
 
 def dict_to_bibtex(entry: dict):
+    """
+    Converts a key-value paired dictionary into a bibtex formatted string.
+    """
     bibtex = "@"+entry['type']+"{"+entry['label']
     for key in sorted(entry):
         if entry[key] is not None and key not in ['type', 'label']:
             bibtex += "\n\t"+key+" = {"+str(entry[key])+"},"
     bibtex = bibtex.strip(',')+"\n}"
     return bibtex
+# }}}
 
 
+# {{{ MAIN
 def main():
     parser = argparse.ArgumentParser(description="Process input arguments.")
     subparsers = parser.add_subparsers(help="sub-command help")
@@ -184,3 +237,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# }}}
