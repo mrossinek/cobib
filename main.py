dee47413bc20920298886a1e8c582fd55a7541ff
mrossinek <rmax@ethz.ch>
2019-04-17 14:04:40 +0200

    Rewrite to query bibtex data directly

--- main.py
+++ main.py
@@ -2,43 +2,30 @@
 import argparse
 import configparser
 import pdftotext
-import json
 import re
 import requests
 import sqlite3
 import sys
 
-API_URL = "https://api.crossref.org/works/"
+HEADER = {'Accept': "application/x-bibtex"}
+API_URL = "https://doi.org/"
 DOI_REGEX = r'(10\.[0-9a-zA-Z]+\/(?:(?!["&\'])\S)+)\b'
-KEYS = {
-        'doi':          "varchar(255) primary key not null",
-        'type':         "varchar(255) not null",
-        'author':       "text",
-        'editor':       "text",
-        'title':        "text",
-        'journaltitle': "varchar(255)",
-        'issue':        "integer",
-        'volume':       "integer",
-        'number':       "integer",
-        'edition':      "integer",
-        'month':        "integer",
-        'year':         "integer",
-        'date':         "date",
-        'url':          "varchar(255)",
-        'isbn':         "integer",
-        'institution':  "varchar(255)",
-        'pages':        "varchar(31)",
-        'file':         "varchar(255)",
-        'abstract':     "text"
+TYPES = {
+        'article': ['author', 'title', 'journal', 'year'],
+        'book': ['author', 'title', 'year'],
+        'collection': ['editor', 'title', 'year'],
+        'proceedings': ['title', 'year'],
+        'report': ['author', 'title', 'type', 'institution', 'year'],
+        'thesis': ['author', 'title', 'type', 'institution', 'year'],
+        'unpublished': ['author', 'title', 'year']
         }
-SYNONYMS = {
-        'doi':          ["DOI"],
-        'title':        ["main-title", "maintitle"],
-        'journaltitle': ["journal", "journaltitle", "publisher"],
-        'date':         ["published-print"],
-        'url':          ["URL"],
-        'isbn':         ["ISBN"],
-        'pages':        ["page"]
+KEYS = {
+        'doi':      "primary key not null",
+        'type':     "not null",
+        'label':    "not null",
+        'file':     "",
+        'tags':     "",
+        'abstract': ""
         }
 
 config = configparser.ConfigParser()
@@ -48,8 +35,12 @@ config.read('config.ini')
 def init(args):
     conn = sqlite3.connect("test.db")
     cmd = "create table literature(\n"
-    for key, value in KEYS.items():
-        cmd += key+' '+value+',\n'
+    for type, keys in TYPES.items():
+        for key in keys:
+            if key not in KEYS.keys():
+                KEYS[key] = ""
+    for key, params in KEYS.items():
+        cmd += key+' text '+params+',\n'
     cmd = cmd[:-2]+'\n)'
     conn.execute(cmd)
     conn.commit()
@@ -57,7 +48,7 @@ def init(args):
 
 def list(args):
     conn = sqlite3.connect("test.db")
-    cursor = conn.execute("SELECT rowid, doi, author, title from literature")
+    cursor = conn.execute("SELECT rowid, doi, label from literature")
     for row in cursor:
         print(row)
 
@@ -73,34 +64,39 @@ def add(args):
             matches = re.findall(DOI_REGEX, text)
             dois.append(most_common(matches))
     if args.doi is not None:
-        dois.extend(args.dois)
+        dois.extend(args.doi)
     for doi in dois:
         assert(re.match(DOI_REGEX, doi))
-        page = requests.get(API_URL+doi, headers=dict(config['HEADER'])).json()
-        payload = {}
-        message = dict(page)['message']
-        for key in message.keys():
-            if key in KEYS.keys():
-                _update_payload(payload, key, message[key])
-            elif key in flatten([v for k, v in SYNONYMS.items()]):
-                _update_payload(payload, key, message[key])
-        print(json.dumps(payload, indent=2))
+        page = requests.get(API_URL+doi, headers=HEADER)
+        parse_bibtex(page.text)
 
 
-def _update_payload(payload, key, value):
-    if key == 'title':  # array of strings
-        payload[key] = value[0]
-    elif key in ['author', 'editor']:  # array of contributors
-        people = ''
-        for person in value:
-            people += person['given'] + ' ' + person['family'] + ', '
-        payload['author'] = people[:-2]
-    elif key in ['date', 'published-print']:  # partial date format
-        payload['year'] = value['date-parts'][0][0]
-        if len(value['date-parts'][0]) > 1:
-            payload['month'] = value['date-parts'][0][1]
-    else:
-        payload[key] = value
+def parse_bibtex(str):
+    # load database info
+    conn = sqlite3.connect("test.db")
+    cursor = conn.execute("PRAGMA table_info(literature)")
+    table_keys = [row[1] for row in cursor]
+
+    # extract information from bibtex
+    lines = str.split('\n')
+    type = re.findall(r'^@([a-zA-Z]*){', lines[0])[0]
+    label = re.findall(r'{(\w*),$', lines[0])[0]
+    keys = 'type,label'
+    values = "'{}','{}'".format(type, label)
+    for line in lines[1:-1]:
+        key, value = line.split('=')
+        key = key.strip()
+        if key not in table_keys:
+            cursor.execute("ALTER TABLE literature ADD COLUMN "+key+" text")
+            cursor = conn.execute("PRAGMA table_info(literature)")
+            table_keys = [row[1] for row in cursor]
+        value = value.strip(' ,{}')
+        keys = keys+','+key
+        values = "{},'{}'".format(values, value)
+    cmd = "INSERT INTO literature ("+keys+") VALUES ("+values+")"
+    cursor.execute(cmd)
+    conn.commit()
+    conn.close()
 
 
 def main():
