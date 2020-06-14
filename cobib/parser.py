ab0343ecd85bef617d761a255e7d43ff363fbca4
mrossinek <rmax@ethz.ch>
2020-06-14 16:49:50 +0200

    Print latex encoding warnings only when adding.
    Latex encoding warnings are only printed when entries are added from
    bibtex sources. This means, that especially on startup, the encoding
    warnings are suppressed.
    
    Note: this may change, when a logger is implemented to log this warnings
    rather than fully suppressing them. (see also #24)
    
    Fixes #29

--- cobib/parser.py
+++ cobib/parser.py
@@ -51,16 +51,17 @@ class Entry:
             if inefficient:
                 return stream.getvalue()
 
-    def __init__(self, label, data):
+    def __init__(self, label, data, suppress_warnings=True):
         """Initializes the Entry object.
 
         Args:
             label (str): Database Id used for this entry.
             data (dict): Dictionary of fields specifying this entry.
+            suppress_warnings (bool): if True, suppresses warnings.
         """
         self._label = label
         self.data = data.copy()
-        self.escape_special_chars()
+        self.escape_special_chars(suppress_warnings)
         if 'FORMAT' in CONFIG.config.keys():
             month_type = CONFIG.config['FORMAT'].get('month', None)
         if month_type:
@@ -125,15 +126,19 @@ class Entry:
             elif isinstance(month, int):
                 self.data['month'] = months[month-1]
 
-    def escape_special_chars(self):
+    def escape_special_chars(self, suppress_warnings=True):
         """Escapes special characters.
 
         Special characters should be escaped to ensure proper rendering in LaTeX documents. This
         function leverages the existing implementation of the pylatexenc module.
+
+        Args:
+            suppress_warnings (bool): if True, suppresses warnings.
         """
         enc = UnicodeToLatexEncoder(non_ascii_only=True,
                                     replacement_latex_protection='braces-all',
-                                    unknown_char_policy='keep')
+                                    unknown_char_policy='keep',
+                                    unknown_char_warning=not suppress_warnings)
         for key, value in self.data.items():
             if isinstance(value, str):
                 self.data[key] = enc.unicode_to_latex(value)
@@ -204,7 +209,7 @@ class Entry:
             database = bibtexparser.load(file, parser=bparser)
         bib = OrderedDict()
         for entry in database.entries:
-            bib[entry['ID']] = Entry(entry['ID'], entry)
+            bib[entry['ID']] = Entry(entry['ID'], entry, suppress_warnings=False)
         return bib
 
     @staticmethod
