53d6b5d9f4684a409acc2b9acabace0261080eb6
mrossinek <rmax@ethz.ch>
2020-07-15 01:10:26 +0200

    If installed from source, append git commit SHA to version info

--- cobib/__init__.py
+++ cobib/__init__.py
@@ -1,2 +1,11 @@
 """CoBib."""
+import os
+import subprocess
+
 __version__ = '2.2.0'
+
+if os.path.exists(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/.git'):
+    # if installed from source, append HEAD commit SHA to version info as metadata
+    proc = subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
+    git_revision, _ = proc.communicate()
+    __version__ += '+' + git_revision.decode('utf-8')[:7]
