4093fc6ff59b8415250c0d94dcecd6f031a5f80d
Max Rossmannek <max.rossmannek@uzh.ch>
2021-10-20 08:45:14 +0200

    Lazily construct YAML instance
    This needs to be done in order to respect the actual runtime setting of
    `config.parsers.yaml.use_c_lib_yaml`. This also fixes the boolean value
    to match the inverted meeting of the config setting compared to the
    keyword argument taken by `ruamel.yaml.YAML`.

--- src/cobib/parsers/yaml.py
+++ src/cobib/parsers/yaml.py
@@ -32,12 +32,18 @@ class YAMLParser(Parser):
 
     name = "yaml"
 
-    _yaml = yaml.YAML(  # type: ignore[attr-defined]
-        typ="safe", pure=config.parsers.yaml.use_c_lib_yaml
-    )
-    _yaml.explicit_start = True
-    _yaml.explicit_end = True
-    _yaml.default_flow_style = False
+    _yaml: Optional[yaml.YAML] = None  # type: ignore[name-defined]
+
+    def __init__(self) -> None:
+        # noqa: D107
+        if YAMLParser._yaml is None:
+            # we need to lazily construct this in order to be able to respect the config setting
+            YAMLParser._yaml = yaml.YAML(  # type: ignore[attr-defined]
+                typ="safe", pure=not config.parsers.yaml.use_c_lib_yaml
+            )
+            YAMLParser._yaml.explicit_start = True
+            YAMLParser._yaml.explicit_end = True
+            YAMLParser._yaml.default_flow_style = False
 
     def parse(self, string: Union[str, Path]) -> Dict[str, Entry]:
         # pdoc will inherit the docstring from the base class
@@ -54,7 +60,7 @@ class YAMLParser(Parser):
                 stream = open(string, "r", encoding="utf-8")  # pylint: disable=consider-using-with
             except FileNotFoundError as exc:
                 raise exc
-        for entry in self._yaml.load_all(stream):
+        for entry in self._yaml.load_all(stream):  # type: ignore[union-attr]
             for label, data in entry.items():
                 bib[label] = Entry(label, data)
         stream.close()
@@ -71,7 +77,9 @@ class YAMLParser(Parser):
 
         LOGGER.debug("Converting entry %s to YAML format.", entry.label)
         stream = io.StringIO()
-        self._yaml.dump({entry.label: dict(sorted(entry.data.items()))}, stream=stream)
+        self._yaml.dump(  # type: ignore[union-attr]
+            {entry.label: dict(sorted(entry.data.items()))}, stream=stream
+        )
         string = stream.getvalue()
 
         string = Event.PostYAMLDump.fire(string) or string
