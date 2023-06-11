9a58966d5a698fccc603317bcc5c17a5ca827a20
Max Rossmannek <max.rossmannek@uzh.ch>
2023-06-11 13:19:47 +0200

    Release v4.1.0

--- src/cobib/__init__.py
+++ src/cobib/__init__.py
@@ -6,7 +6,7 @@
 import subprocess
 from pathlib import Path
 
-__version__ = "4.0.0"
+__version__ = "4.1.0"
 
 if (Path(__file__).parent.parent.parent / ".git").exists():
     # if installed from source, append HEAD commit SHA to version info as metadata
